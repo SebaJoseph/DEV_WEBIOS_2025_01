44ab5fbaed66758ee2351cefd56496aba6452dd8
SebaJoseph <josephsebastien20032008@gmail.com>
2025-06-03 21:44:01 -0300

    iniciou da aula

---
+++ JS/JSmain.js
@@ -0,0 +1,32 @@
+var idade
+const ano  = 2010
+//JavaScript
+console.log("Helo world") //apresentar uma mensagem
+console.clear() //limpar
+//variaveis
+var numero1 =192 // inicializar uma variavel
+console.log(numero1)
+
+var num1 = 4
+var num2 = 4
+var resultado = num1 + num2
+console.log(resultado)
+
+idade = 89 // iniciar a variavel em outra parte do codigo
+console.log(idade)
+
+
+ ano = 1936
+ 
+
+//Textos - strings
+var nome = 'Matheus Araujo'
+var nome2 = "Matheus Vinicius"
+var nome3 = "12365.4789" // texto
+console.log(nome, nome2, nome3)
+
+VAR // Flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
+
+LET // você não pode recriar a mesma em outras partes do codigo com o mesmo nome, mas pode chamar e alterar o valor inicial
+
+CONST // você não pode recriar a mesma em outras partes do cogido e não pode alterar o valor inicial dela garantindo a confiança do dado que está sendo utilizadado no decorrer do seu codigo
