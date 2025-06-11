7f5aedaa66f29044de361597fc1d916a7431af23
SebaJoseph <josephsebastien20032008@gmail.com>
2025-06-10 21:33:54 -0300

    aula 02 inicio

--- JS/JS01-Introducao/JSmain.js
+++ JS/JS01-Introducao/JSmain.js
@@ -26,6 +26,7 @@ var nome3 = "12365.4789" // texto
 console.log(nome, nome2, nome3)
 
 
+
 console.log("olá + papai") // concatenar
 console.log("Aluno" + nome + "e Aluno" + nome2 + 'tem um total de ' + nome3 + "dinheiro ") // concatenar
 //  VAR // Flexivel e com menos regras (posso alterar o valor inicial de uma var e recriar a mesma em outras partes do codigo)
@@ -40,6 +41,7 @@ console.log(`Aluno ${nome} e Aluno ${nome2}tem um total de ${nome3}dinheiros`) /
 // Concatenar 3
 
 
+
 // Operadores
 var valor1 = 5
 var valor2 = 5
@@ -48,6 +50,7 @@ console.log(valor1 - valor2)
 console.log(valor1 * valor2)
 console.log(valor1 / valor2)
 console.log(valor1 ** valor2)
+
 
 
 // Operadores relacionais
@@ -61,9 +64,35 @@ console.log(true == false)
 console.log(true === 'true') // boolean e a outro e txt > String
 console.log(true != 'false') // true
 
+
 let temcamisa = false
 let temCracha = true
 console.log(temcamisa)
 console.log(temCracha)
-console.log(!(temcamisa  == true && temCracha == true) // false == true = false)
+console.log(temcamisa  == true && temCracha == true) // false == true = false)
 console.log(temCamise == true || temCracha == true)// false ou true = true
+
+
+//operador de atribuição
+var x = 3
+var y =2
+//x = x + mmj
+// x += y formato contraido
+// x = 3 + 2
+console.log(x)
+console.log(x)
+
+x = 720
+console.log(x)
+
+// log > para mostrar resultados, mensagens do sistema
+//clear > Limpar o console e deixar disponivel para novas mensagens
+
+console.clear()
+
+console.log("Está tudo limpo")
+console.log("olá mundo")
+
+console.error("Não use essa tela, você poder ser punido isso")
+
+console.warn("mas apresenta uma mensagem em amarelo")
---
+++ JS/js02-string/main.js
@@ -0,0 +1,38 @@
+1 // "A" // Caracter
+// "ola" // Texto - Sting
+
+var texto1 = "olá Ios"
+//0 1 2 3 4 5 6 
+
+var resultado1 = texto1.chartAt(6)
+console.log(resultado1)
+var resultado2 = texto1{1} // acessar caracter
+console.log(resultado2)
+
+var lorem = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+
+
+
+console.log(texto2.length)
+// console.log(lorem. length >= 245)
+
+var texto2 = olá Tec de Tecnologia
+var texto3 = "olá meu nome e ia, seu amigo"
+
+//Formato1
+var resultado3 = texo.2 tolowercase
+console. log(resultado3)
+//formato 2
+console.log(texto3.toUppercase)
+
+
+var texto4 = 'Mozilla'
+// oz 0 2 3 4 4 5
+console.log(texto4. 
+    
+    
+    
+    
+    var texto5 = Ben vindp a Microsoft
+    let novaMengagem = texto5.replace('Microsoft')
+    console.log(novaMensagem)
